9c4f22881d6c4862ab5ac0c4bb5350923470863b
yashr6577 <yashr6577@gmail.com>
2024-11-25 16:56:04 +0530

    changing https to http

--- src/components/forms/SignInForm.jsx
+++ src/components/forms/SignInForm.jsx
@@ -27,7 +27,7 @@ const SignInForm = () => {
     setLoading(true); // Start loading animation
 
     try {
-      const response = await fetch("https://localhost:5000/login", {
+      const response = await fetch("http://localhost:5000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
